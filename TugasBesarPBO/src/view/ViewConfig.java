839a25e74d463561fd6184da9e0b77298d201bd7
Albertus A <angkuwjr@yahoo.com>
2020-10-20 03:20:14 +0700

    Make GUI Development (JPanel,Dashboard,ViewConfig)

---
+++ TugasBesarPBO/src/view/ViewConfig.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import java.awt.*;
+
+/**
+ *
+ * @author 1119002 Albertus Angkuw
+ */
+public interface ViewConfig {
+    Color BGCOLOR_DEFAULT = new java.awt.Color(67, 50, 103);
+    Font FONT_DEFAULT_BOLD = new java.awt.Font("Segoe UI", 1, 14);
+    Font FONT_DEFAULT_PLAIN = new java.awt.Font("Segoe UI", 0, 14);
+}
